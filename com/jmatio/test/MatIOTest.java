5e181894d50db1e153e7be1cd41ef48e900600f4
Wojciech Gradkowski <wgradkowski@users.sf.net>
2006-04-23 17:32:51 +0000

    + MLDouble test added
    git-svn-id: https://jmatio.svn.sourceforge.net/svnroot/jmatio/trunk@16 864c2297-9b29-0410-a227-9d78e99c1ec9

--- com/jmatio/test/MatIOTest.java
+++ com/jmatio/test/MatIOTest.java
@@ -4,9 +4,9 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import junit.framework.JUnit4TestAdapter;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import org.apache.log4j.Logger;
 
@@ -14,6 +14,7 @@ import com.jmatio.io.MatFileReader;
 import com.jmatio.io.MatFileWriter;
 import com.jmatio.types.MLArray;
 import com.jmatio.types.MLChar;
+import com.jmatio.types.MLDouble;
 
 /**
  * The test suite for JMatIO
@@ -22,7 +23,7 @@ import com.jmatio.types.MLChar;
  */
 public class MatIOTest
 {
-    
+    static Logger logger = Logger.getLogger( MatIOTest.class );
     /**
      * Tests <code>MLChar</code> reading and writing.
      * 
@@ -30,13 +31,20 @@ public class MatIOTest
      */
     @Test public void MLCharArrayTest() throws IOException
     {
-        MLChar mlChar = new MLChar("chararr", "dummy");
-        
+        //array name
+        String name = "chararr";
+        //file name in which array will be storred
+        String fileName = "mlchar.mat";
+        //temp
         String valueS;
+
+        //create MLChar array of a name "chararr" containig one
+        //string value "dummy"
+        MLChar mlChar = new MLChar(name, "dummy");
         
         //get array name
         valueS = mlChar.getName();
-        assertEquals("MLChar name getter", "chararr", valueS);
+        assertEquals("MLChar name getter", name, valueS);
         
         //get value of the first element
         valueS = mlChar.getString(0);
@@ -47,17 +55,65 @@ public class MatIOTest
         list.add( mlChar );
         
         //write arrays to file
-        new MatFileWriter("mlchar.mat", list);
+        new MatFileWriter( fileName, list );
         
         //read array form file
-        MatFileReader mfr = new MatFileReader("mlchar.mat");
-        MLArray mlCharRetrived = mfr.getMLArray( "chararr" );
+        MatFileReader mfr = new MatFileReader( fileName );
+        MLArray mlCharRetrived = mfr.getMLArray( name );
         
         assertEquals("Test if value red from file equals value stored", mlChar, mlCharRetrived);
         
         //try to read non existent array
         mlCharRetrived = mfr.getMLArray( "nonexistent" );
         assertEquals("Test if non existent value is null", null, mlCharRetrived);
+    }
+    
+    /**
+     * Tests <code>MLDouble</code> reading and writing.
+     * 
+     * @throws IOException
+     */
+    @Test public void MLDoubleArrayTest() throws IOException
+    {
+        //array name
+        String name = "doublearr";
+        //file name in which array will be storred
+        String fileName = "mldouble.mat";
+
+        //test column-packed vector
+        double[] src = new double[] { 1.0, 2.0, 3.0, 4.0, 5.0, 6.0 };
+        //test 2D array coresponding to test vector
+        double[][] src2D = new double[][] { { 1.0, 4.0 },
+                                            { 2.0, 5.0 },
+                                            { 3.0, 6.0 }
+                                        };
+
+        //create 3x2 double matrix
+        //[ 1.0 4.0 ;
+        //  2.0 5.0 ;
+        //  3.0 6.0 ]
+        MLDouble mlDouble = new MLDouble( name, src, 3 );
+        
+        //write array to file
+        ArrayList<MLArray> list = new ArrayList<MLArray>();
+        list.add( mlDouble );
+        
+        //write arrays to file
+        new MatFileWriter( fileName, list );
+        
+        //read array form file
+        MatFileReader mfr = new MatFileReader( fileName );
+        MLArray mlArrayRetrived = mfr.getMLArray( name );
+        
+        //test if MLArray objects are equal
+        assertEquals("Test if value red from file equals value stored", mlDouble, mlArrayRetrived);
+        
+        //test if 2D array match
+        for ( int i = 0; i < src2D.length; i++ )
+        {
+            boolean result = Arrays.equals( src2D[i], ((MLDouble)mlArrayRetrived ).getArray()[i] );
+            assertEquals( "2D array match", true, result );
+        }
     }
     
     public static junit.framework.Test suite()
